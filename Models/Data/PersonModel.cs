c797f560ec453c1ff21cc6d69fbd7affce1768b7
Craig Roberts <craig.roberts11@outlook.com>
2024-04-08 20:16:08 +0930

    Added person model for intervention orders

---
+++ Models/Data/PersonModel.cs
@@ -0,0 +1,20 @@
+﻿using CourtSystem.Models.UI;
+using System.ComponentModel.DataAnnotations;
+
+namespace CourtSystem.Models.Data;
+
+public class PersonModel {
+    [Key]
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public DateTime DateOfBirth { get; set; }
+
+    public Person ToUIModel() {
+        return new Person {
+            FirstName = FirstName,
+            LastName = LastName,
+            DateOfBirth = DateOfBirth
+        };
+    }
+}
