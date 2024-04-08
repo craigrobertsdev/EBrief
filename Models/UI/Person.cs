2a2eda61fcbb435e61161f3a62b8374582a7cb0c
Craig Roberts <craig.roberts11@outlook.com>
2024-04-08 20:17:01 +0930

    Added person model to match data model

---
+++ Models/UI/Person.cs
@@ -0,0 +1,7 @@
+﻿namespace CourtSystem.Models.UI;
+
+public record Person {
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public DateTime DateOfBirth { get; set; }
+}
