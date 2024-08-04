06b5d318094e7af22d46ba44ae6bafb59d477910
Craig Roberts <craig.roberts11@outlook.com>
2024-08-04 20:07:39 +0930

    Updated to fit new CourtListModel structure

--- EBrief.Shared/Models/Data/CaseFileModel.cs
+++ EBrief.Shared/Models/Data/CaseFileModel.cs
@@ -14,10 +14,14 @@ public class CaseFileModel
     public Guid CourtListId { get; set; }
     public string CaseFileNumber { get; set; } = string.Empty;
     public DefendantModel Defendant { get; set; } = default!;
+    public string DefendantAppearanceMethod { get; set; } = string.Empty;
+    public string[] OffenceDetails { get; set; } = [];
+    public DefenceCounsel? Counsel { get; set; }
     [MaxLength(20)]
     public string? CourtFileNumber { get; set; } = string.Empty;
     public int ListNumber { get; set; }
     public string ListingType { get; set; } = string.Empty;
+    public string HearingType { get; set; } = string.Empty;
     public List<HearingEntryModel> Schedule { get; set; } = [];
     public List<CaseFileEnquiryLogEntryModel> CfelEntries { get; set; } = [];
     public string FactsOfCharge { get; set; } = default!;
---
+++ EBrief.Shared/Models/Data/DefenceCounsel.cs
@@ -0,0 +1,8 @@
+﻿namespace EBrief.Shared.Models.Data;
+
+public class DefenceCounsel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Number { get; set; } = string.Empty;
+}
