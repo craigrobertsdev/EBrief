6529d4a0c11c02959a757f064d5ce5f353c088a2
Craig Roberts <craig.roberts11@outlook.com>
2024-11-19 05:04:58 +1030

    Added HomeView, styled buttons, started HomeViewModel

--- EBrief/ViewModels/HomeViewModel.cs
+++ EBrief/ViewModels/HomeViewModel.cs
@@ -1,4 +1,16 @@
-﻿namespace EBrief.ViewModels;
+﻿using EBrief.Commands;
+using EBrief.Services;
+using System.Windows.Input;
+
+namespace EBrief.ViewModels;
 public class HomeViewModel : ViewModelBase
 {
+    public ICommand AddNewCourtListCommand { get; }
+    public ICommand LoadPreviousCourtListCommand { get; }
+    public ICommand OpenCourtListFromFileCommand { get; }
+
+    public HomeViewModel(INavigationService modalNavigationService, INavigationService courtListNavigationService)
+    {
+        AddNewCourtListCommand = new AddNewCourtListCommand(modalNavigationService, courtListNavigationService);
+    }
 }
